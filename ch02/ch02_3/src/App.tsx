82e9fc777c161ffdd80f5a86ae76e317cbba0f49
devqw010102 <qw010102@gmail.com>
2026-01-27 16:01:47 +0900

    Add: chap02

---
+++ ch02/ch02_3/src/App.tsx
@@ -0,0 +1,12 @@
+import {Component} from 'react'
+import ArrowComponent from './ArrowComponent'
+import ClassComponent from './ClassComponent'
+
+export default function App() {
+  return (
+    <ul>
+      <ClassComponent href="http://www.google.com" text="go to Google" />
+      <ArrowComponent href="http://www.twitter.com" text="go to Twitter" />
+    </ul>
+  )
+}
