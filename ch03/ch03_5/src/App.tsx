90473bd4eacb90ee6a4f7b27e645cdb11457454b
devqw010102 <qw010102@gmail.com>
2026-01-28 15:21:00 +0900

    Add: chap03

---
+++ ch03/ch03_5/src/App.tsx
@@ -0,0 +1,13 @@
+import ButtonTest from './pages/ButtonTest'
+import InputTest from './pages/InputTest'
+import ModalTest from './pages/ModalTest'
+
+export default function App() {
+  return (
+    <main data-theme="light" className="min-h-screen p-4 bg-white">
+      <ButtonTest />
+      <InputTest />
+      <ModalTest />
+    </main>
+  )
+}
